65faa5e7b86d2b623a2ab8a7c55904e1c23e8c41
lukfd <luca.comba98@gmail.com>
2022-01-16 19:28:04 -0600

    modify the voting buttons.

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -13,12 +13,18 @@ function Body() {
                 <Col xs={10}>
                     <Card>
                         {/* <Card.Header>Proposal Type</Card.Header> */}
-                        <Card.Body>
-                            <Card.Title>Title</Card.Title>
-                            <Card.Text>
-                            With supporting text below as a natural lead-in to additional content.
-                            </Card.Text>
-                            <Button className="vote-button">Vote</Button>
+                        <Card.Body className="p-5">
+                            <div className="p-5">
+                                <Card.Title>Title</Card.Title>
+                                <Card.Text>
+                                With supporting text below as a natural lead-in to additional content.
+                                </Card.Text>
+                            </div>
+                            <hr />
+                            <div className="text-center">
+                                <Button className="vote-button-yes mx-2">Yes</Button> 
+                                <Button className="vote-button-no mx-2">No</Button> 
+                            </div>
                         </Card.Body>
                     </Card>
                 </Col>
