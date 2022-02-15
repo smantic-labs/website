26f1c3ae0d9589c97b1d4662f41e0d0225232a77
lukfd <luca.comba98@gmail.com>
2022-02-14 22:31:59 -0600

    moved setting the address to the app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
+import { React, useState, useEffect } from "react"
+
 // Our components
 import Header from './components/Header/Header'
 import Body from './components/Body/Body'
 import Footer from './components/Footer/Footer'
+import LoginModal from './components/LoginModal/LoginModal'
 
-// Bootstrap
+async function isLoggedIn(setAddress) {
+    // does not have metamask
+    if (!window.ethereum) {
+        alert("Get MetaMask!");
+        return;
+    }
+
+    const accounts = await window.ethereum.request({
+        method: "eth_accounts",
+    });
+
+    if (accounts.length > 0) {
+        setAddress(accounts[0])
+        return
+    }
+}
 
 function App() {
-  return (
-    <>
-      <Header />
-      <Body />
-      <Footer />
-    </>
-  );
+    const [address, setAddress] = useState("");
+
+    useEffect(() => {
+        isLoggedIn(setAddress);
+    }, []);
+
+    return address ? (
+         <>
+            <Header address={address} />
+            <Body />
+            <Footer />
+        </>
+    ) : (
+        <LoginModal setAddress={setAddress} />
+    )
 }
 
 export default App;
