172f03b2e7641d7f70ed67f2f862a13ed47bcc62
lukfd <luca.comba98@gmail.com>
2022-02-21 19:18:54 -0600

    Substituted our names with github link in footer.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,23 +1,27 @@
 import { Col, Container, Row } from "react-bootstrap";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faDiscord } from '@fortawesome/free-brands-svg-icons'
+import { faDiscord, faGithub } from '@fortawesome/free-brands-svg-icons'
 
 function Footer() {
     return (
         <Container className="w-100 p-3 text-center fixed-bottom">
             <Row>
                 <Col>
-                    <FontAwesomeIcon icon={faDiscord} size="lg" />
+                    <a href="https://github.com/muichiro-mc" style={{ color: 'black' }}>
+                        <FontAwesomeIcon icon={faDiscord} size="lg" />
+                    </a>
                 </Col>
                 <Col>
                     <small>2022</small>
                 </Col>
                 <Col>
-                    <small>Tyler and Luca</small>
+                    <a href="https://github.com/muichiro-mc" style={{ color: 'black' }}>
+                        <FontAwesomeIcon icon={faGithub} size="lg" />
+                    </a>
                 </Col>
             </Row>
         </Container>
     );
 }
-  
+
 export default Footer;
