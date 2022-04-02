d8df3af320e696a710c8b392f1109576619fd707
smantic <tyler@smantic.dev>
2022-04-01 23:12:46 -0500

    get proposals

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,32 +1,71 @@
 import "../../style/index.css"
 
+import { React, useState } from "react"
 import ProposalCard from "./ProposalCard/ProposalCard"
 import { Row, Col, Carousel } from "react-bootstrap";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronCircleLeft, faChevronCircleRight } from "@fortawesome/free-solid-svg-icons"
 
-function Body({ address }) {
+
+//import ProposalCard from "../ProposalCard/index";
+
+
+const ProposalTypes = ["ImmediateBan", "Ban", "Unban", "Whitelist", "ResetWorld"]
+
+let inital = [null, null, null, null, null, null, null, null]
+
+function Body(props) {
+
+    let { Contract, Provider, Address} = props;
+
+    const [proposals, setProposals ] = useState(inital)
+
     const nextIcon = <FontAwesomeIcon icon={faChevronCircleRight} size="lg" className="fa-shadow" />
     const prevIcon = <FontAwesomeIcon icon={faChevronCircleLeft} size="lg" className="fa-shadow" />
-    const data = [
-        { title: "Title 1", body: "With supporting text below as a natural lead-in to additional content." },
-        { title: "Title 2", body: "With supporting text below as a natural lead-in to additional content." },
-        { title: "Title 3", body: "With supporting text below as a natural lead-in to additional content." }
-    ];
 
+    if (proposals[0] == null && Contract != null) { 
+
+        (async() => { 
+            for (let i = 0; i < 8; i++){ 
+                proposals[i] = await Contract.functions.proposals(i);
+            }
+            setProposals(proposals);
+        })()
+    }
+
+    console.log(proposals)
     return (
-        //data.map((obj, index) => { return <ProposalCard />})
         <Row className="justify-content-md-center">
             <Col md={6}>
                 <Carousel interval={null} indicators={false} variant={'dark'} nextIcon={nextIcon} prevIcon={prevIcon}>
-                    {data.map((item, index) => {
-                        return (
-                            <Carousel.Item key={index}>
-                                <ProposalCard address={address} title={item.title} body={item.body}/>
-                            </Carousel.Item>
-                        )
-                    })}
+                    {
+                        proposals.map((proposal, index) => {
+
+                            if (proposal == null  || proposal[0] == null ) { 
+                                return (
+                                    <Carousel.Item key={index}>
+                                        <ProposalCard 
+                                            address={Address} 
+                                            title={"Placeholder title"} 
+                                            body={"Placeholder body"}/>
+                                    </Carousel.Item>
+                                )
+                            }
+
+                            var title = ProposalTypes[proposal.kind]
+                            var body = "placeholder proposal body"
+
+                            return (
+                                <Carousel.Item key={index}>
+                                    <ProposalCard 
+                                        address={Address} 
+                                        title={title} 
+                                        body={body}/>
+                                </Carousel.Item>
+                            )
+                        })
+                    }
                 </Carousel>
             </Col>
         </ Row>
