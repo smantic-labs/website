14c2e660bdd12690ec5237c791b22cb424142550
lukfd <luca.comba98@gmail.com>
2022-07-14 23:31:54 -0500

    Adding sitchEthereumChain function to switch to ropsten network.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { React, useState } from "react"
+import { ethers } from "ethers"
 
 // Our components
 import Header from './components/Header/Header'
 import Body from './components/Body/Body'
 import Footer from './components/Footer/Footer'
-import { ethers } from "ethers"
 import abi from "./abi"
 
 const contract_address = "0x3174B73CE6935161fc6d80219fC35558D8928177"
@@ -12,9 +12,9 @@ const infura_project_id = "398f95cb8ffc4a18820d767955580aea"
 const network = "ropsten"
 
 
-let initalState = { 
-    Provider: null, 
-    Account: null, 
+let initalState = {
+    Provider: null,
+    Account: null,
     Contract: null,
 }
 
@@ -23,42 +23,55 @@ function App() {
 
     const [eth, setEth] = useState(initalState)
 
-    if (window.ethereum == null) { 
+    if (window.ethereum == null) {
         // TODO: use backend server or infura reverse proxy. 
         alert("metamask is required currently!!");
-        return; 
+        return;
     }
 
-    if (eth.Provider == null) { 
-       (async () => { 
+    if (eth.Provider == null) {
+        (async () => {
 
-            var account 
-            var provider 
+            var account
+            var provider
 
-            if( window.ethereum != null) {
+            if (window.ethereum != null) {
                 // first get the available eth accounts. 
                 var accounts = await window.ethereum.request({
                     method: "eth_accounts",
                 });
-
 
                 // sign in if needed
                 if (accounts.length === 0) {
                     accounts = await window.ethereum.request({
                         method: "eth_requestAccounts",
                     });
-                } 
+                }
+
+                // switch to ROPSTEN network
+                // Find chain id: https://chainlist.org/ or https://chainid.network/chains.json
+                // Full documentation: https://docs.metamask.io/guide/rpc-api.html#wallet-switchethereumchain
+                try {
+                    await window.ethereum.request({
+                        method: "wallet_switchEthereumChain",
+                        params: [{ chainId: `0x${Number(3).toString(16)}` }]
+                    });
+                } catch (error) {
+                    // here we could have a modal or popup to retry the switch to the network
+                    alert(`${error.message} Try to refresh the page.`)
+                }
+
                 account = accounts[0]
                 provider = new ethers.providers.Web3Provider(window.ethereum)
-            } else { 
+            } else {
                 // use infura if there is no metamask.
                 provider = new ethers.providers.InfuraProvider(network, infura_project_id)
             }
 
-            setEth({ 
+            setEth({
                 Provider: provider,
                 Contract: new ethers.Contract(contract_address, abi, provider),
-                Account: account, 
+                Account: account,
             })
         })()
     }
@@ -66,14 +79,14 @@ function App() {
     return (
         <>
             <Header
-              Account={eth.Account}
-              Contract={eth.Contract}
-              Provider={eth.Provider}
+                Account={eth.Account}
+                Contract={eth.Contract}
+                Provider={eth.Provider}
             />
-            <Body 
-              Account={eth.Account}
-              Contract={eth.Contract}
-              Provider={eth.Provider}
+            <Body
+                Account={eth.Account}
+                Contract={eth.Contract}
+                Provider={eth.Provider}
             />
             <Footer />
         </>
