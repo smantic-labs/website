db6650a64063c68837a8b1ed080000466a290f03
lukfd <luca.comba98@gmail.com>
2022-11-19 22:18:06 -0600

    Changed footer to have icons on left side.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,18 +4,13 @@ import { faDiscord, faGithub } from '@fortawesome/free-brands-svg-icons'
 
 function Footer() {
     return (
-        <Container className="w-100 p-3 text-center fixed-bottom">
+        <Container className="w-100 p-3 fixed-bottom">
             <Row>
                 <Col>
-                    <a href="https://github.com/muichiro-mc" style={{ color: 'black' }}>
+                    <a href="https://github.com/muichiro-mc" style={{ color: 'black' }} className="px-3">
                         <FontAwesomeIcon icon={faDiscord} size="lg" />
                     </a>
-                </Col>
-                <Col>
-                    <small>2022</small>
-                </Col>
-                <Col>
-                    <a href="https://github.com/muichiro-mc" style={{ color: 'black' }}>
+                    <a href="https://github.com/muichiro-mc" style={{ color: 'black' }} className="px-3">
                         <FontAwesomeIcon icon={faGithub} size="lg" />
                     </a>
                 </Col>
