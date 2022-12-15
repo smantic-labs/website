e1bbcbf6eabd1fd98786b6a809e74d0525ad7bda
lukfd <luca.comba98@gmail.com>
2022-12-14 22:08:22 -0600

    Generate proposal cards.

--- src/components/Body/Carousel.js
+++ src/components/Body/Carousel.js
@@ -4,46 +4,45 @@ import { faChevronCircleLeft, faChevronCircleRight } from "@fortawesome/free-sol
 import { Carousel } from "react-bootstrap";
 import ProposalCard from "./ProposalCard/ProposalCard"
 
-
 const ProposalTypes = ["ImmediateBan", "Ban", "Unban", "Whitelist", "ResetWorld"]
 let inital = []
 
 function ProposalCarousel(props) { 
 
-    let { Contract, Provider, Address} = props;
+    let { Contract, Address } = props;
     const [proposals, setProposals ] = useState(inital)
+    let items = [] 
 
-    if (Contract != null && proposals.length == 0) { 
-        (async() => { 
+    if (Contract != null && proposals.length === 0) { 
+        (async() => {
             let promises = []
-            for (let i = 0; i < 8; i++){ 
+            let totalProposals = await Contract.functions.totalProposals()
+            for (let i = 1; i <= totalProposals[0].toNumber(); i++) {
                 promises.push(Contract.functions.proposals(i))
             }
-
-            setProposals(await Promise.all(promises));
+            setProposals(await Promise.all(promises))
         })()
     }
 
     const nextIcon = <FontAwesomeIcon icon={faChevronCircleRight} size="lg" className="fa-shadow" />
     const prevIcon = <FontAwesomeIcon icon={faChevronCircleLeft} size="lg" className="fa-shadow" />
 
-    let items = [] 
-
     for(let i = 0; i < proposals.length; i++){ 
         let proposal = proposals[i]
-        // console.log(proposal)
-        items.push(
-          <Carousel.Item key={i}>
-              <ProposalCard 
-                  address={Address} 
-                  title={ProposalTypes[proposal.kind]} 
-                  body={"Placeholder body"}
-                  kind={proposal.kind}
-                  inFavorCount={parseInt(proposal.pro._hex)}
-                  startTime={parseInt(proposal.startTime._hex)}
-            />
-          </Carousel.Item>
-        );
+        if (proposal[0] > 0) {
+            items.push(
+            <Carousel.Item key={i}>
+                <ProposalCard 
+                    address={Address} 
+                    title={ProposalTypes[proposal.kind]} 
+                    body={"Placeholder body"}
+                    kind={proposal.kind}
+                    inFavorCount={parseInt(proposal.pro._hex)}
+                    startTime={parseInt(proposal.startTime._hex)*1000}
+                />
+            </Carousel.Item>
+            );
+        }
     }
 
     return (
